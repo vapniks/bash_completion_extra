fb9f3ccb9c03d233c7aa158c8b332397ff393563
Joe Bloggs <vapniks@yahoo.com>
2014-02-03 20:31:50 +0000

    Added note about nospace & filenames options

--- bash_completion_extra.sh
+++ bash_completion_extra.sh
@@ -24,6 +24,8 @@
 # other functions in that file, so you should make sure that /etc/bash_completion has been sourced first.
 # The function should not be used directly (it takes different arguments to normal completion functions),
 # but should be called by another completion function.
+# NOTE: you probably want to use the nospace and filenames options with complete, e.g:
+#   complete -o nospace -o filenames -F _myfunc mycmd
 # @param $1  The directory to start in.
 # @param $2  If `-d', complete only on directories.  Otherwise filter/pick only
 #            completions with `.$1' and the uppercase version of it as file
